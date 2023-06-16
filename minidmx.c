9d504f24231be3b887c401eeaf1b429a94a7e4ae
Roger Wolff <R.E.Wolff@BitWizard.nl>
2023-06-16 15:09:12 +0200

    code cleanup

--- minidmx.c
+++ minidmx.c
@@ -9,7 +9,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stropts.h>
+//#include <stropts.h>
+//  sys/ioctl clashes with asm/termios. That one is required, this just generates a warning. 
 //#include <sys/ioctl.h>
 #include <asm/termbits.h>
 #include <errno.h>
@@ -22,29 +23,42 @@
 
 unsigned char buf[DMXSIZE+1];
 
+int led=1; 
+
 void update_dmx_universe (unsigned char *dmx)
 {
   static int t;
-  if (t++ < 15) return;
+
+  t++;
+#if 1
+  if (t < 30) dmx[led] = 15 + t * 5;
+  else        dmx[led] = 15 + (60-t) * 5;
+#endif
+//  printf ("%d\n", dmx[led]);
+
+  if ((t != 30) && (t != 60)) return;
+  if (t >= 60) {
+     t = 0;
+//     led++;
+ //    printf ("led=%d\n", led);
+  }
   static int p;
 
-  t = 0;
 #define M 4
   p = (p+1) % M;
 
   for (int i =1;i<DMXSIZE;i++) 
-    dmx[i] = ((i % M) == p)?255:0 ;
+    if (i != led)
+      dmx[i] = ((i % M) == p)?255:0 ;
+
 }
 
-
-
-int main (int argc, char **argv)
+int setup_dmx_uart (char *port) 
 {
   static const int rate = 250000;
-  int fd = 0;
   struct termios2 tio;  // linux-specific terminal stuff
-  char *port = argv[1];
-
+  int fd;
+ 
   if (!port) {
     port = "/dev/ttyAMA0";
   }
@@ -56,10 +70,9 @@ int main (int argc, char **argv)
 
   if (ioctl(fd, TCGETS2, &tio) < 0) {
     printf  ("Failed to get current serial port settings");
-    return 1;
+    exit (1);
   }
   printf ("Port speeds before change %d/%d\n" , tio.c_ispeed,  tio.c_ospeed );
-
 
   tio.c_cflag |= CLOCAL;    // port is local, no flow control
   tio.c_cflag &= ~CSIZE;
@@ -76,34 +89,50 @@ int main (int argc, char **argv)
 
   if (ioctl(fd, TCSETS2, &tio) < 0) {
     printf ("Failed to update serial port settings");
-    return 1;
+    exit (1);
   }
 
   if (ioctl(fd, TCGETS2, &tio) < 0) {
     printf ("Error getting altered settings from port");
+    exit (1);
   } else {
     printf ("Port speeds are %d/%d\n" , tio.c_ispeed,  tio.c_ospeed );
   }
   buf [0] = 0;
+  return fd;
+}
+
+void send_dmx_frame (int fd, unsigned char *buf)
+{
+  if (ioctl(fd, TIOCSBRK, NULL) < 0) {
+    printf (" ioctl() failed");
+    exit (1);
+  }
+  usleep (100);
+  if (ioctl(fd, TIOCCBRK, NULL) < 0) {
+    printf (" ioctl() failed");
+    exit (1);
+  }
+  usleep (100);
+  if (write (fd, buf, DMXSIZE+1) < DMXSIZE) 
+    perror ("write");
+}
+
+
+int main (int argc, char **argv)
+{
+  int fd = 0;
+//  char *port = argv[1];
+//  char *port = NULL;
+
+  if (argc > 1) led = atoi (argv[1]);
+  fd = setup_dmx_uart (NULL);
 
   while (1) {
-    if (ioctl(fd, TIOCSBRK, NULL) < 0) {
-      printf (" ioctl() failed");
-      exit (1);
-    }
-    usleep (100);
-    if (ioctl(fd, TIOCCBRK, NULL) < 0) {
-      printf (" ioctl() failed");
-      exit (1);
-    }
-    
-    usleep (100);
-    if (write (fd, buf, DMXSIZE+1) < DMXSIZE) 
-      perror ("write");
+    send_dmx_frame (fd, buf);
     update_dmx_universe (buf);
-    usleep (30000);
+    usleep (15000);
 
   }
-
- 
+  exit (0); 
 }
